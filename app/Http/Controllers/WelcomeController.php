0bf2aca0f4381e6d507cd3a35d14c39e115ba7ec
will's home <allerwill.lee@gmail.com>
2019-10-19 14:00:32 +0700

    Created Model: Product, Manufacture
    Manufacture deleted in DB
    Upload db 'COP' to edit

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Product;
+use app\Manufacture;
 
 class WelcomeController extends Controller
 {
-    public function index($ID = 'index'){
+    public function index($ID = 'index')
+    {
+//        $products = Product::all();
+//        return view('welcome', ['Product' => $products]);
         return view($ID);
     }
+
 
 }
