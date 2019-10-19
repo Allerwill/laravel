6654d64e1bb9aef85c6ebfbf5e2e6af5e5f3e049
will's home <allerwill.lee@gmail.com>
2019-10-19 14:14:14 +0700

    Commit merge -> Truong

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,24 +6,10 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     public function index($ID = 'index'){
         //$product = Product::all();
         //return view('welcome', ['product' => $product]);
-=======
-    public function index($ID = 'index')
-    {
-//        $products = Product::all();
-//        return view('welcome', ['Product' => $products]);
->>>>>>> 0bf2aca0f4381e6d507cd3a35d14c39e115ba7ec
-=======
-    public function index($ID = 'index'){
->>>>>>> parent of 0bf2aca... Created Model: Product, Manufacture
-=======
-    public function index($ID = 'index'){
->>>>>>> parent of 0bf2aca... Created Model: Product, Manufacture
         return view($ID);
     }
 
